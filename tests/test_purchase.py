801fd1128fcce380ae38668b57f4e5a5a5f10d84
Danilo Caires Martinez Vieira <danilocaires@outlook.com>
2024-05-07 20:52:22 -0300

    New scripts added

---
+++ tests/test_purchase.py
@@ -0,0 +1,13 @@
+import utils.general
+import pages.inventory
+
+def test_purchase_single_ok(driver):
+    utils.general.login(driver,'standard_user','secret_sauce')
+    titles = driver.find_elements(pages.inventory.links_producttitles)
+
+def test_purchase_multiple_ok():
+    pass
+
+def test_purchase_checkout_empty_fields():
+    pass
+
---
+++ tests/test_sortby.py
@@ -0,0 +1,12 @@
+def test_sortby_az():
+    pass
+
+def test_sortby_za():
+    pass
+
+def test_sortby_highlow():
+    pass
+
+def test_sortby_lowhigh():
+    pass
+
